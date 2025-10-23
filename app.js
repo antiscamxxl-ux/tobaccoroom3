42e0ac69b429469191586b7e1f64ebd6d5288b86
antiscamxxl-ux <antiscam.xxl@gmail.com>
2025-10-24 02:13:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ try {
     console.error("Telegram Web App SDK is not available.");
 }
 
-const BACKEND_URL = 'https://antiscamxxl-ux.github.io/tobaccoroom3/'; // ЗАМЕНИТЕ НА ВАШ URL
+const BACKEND_URL = 'https://antiscamxxl-ux.github.io/tobaccoroom3'; // ЗАМЕНИТЕ НА ВАШ URL
 
 // Элементы
 const welcomeScreen = document.getElementById('welcome-screen');
@@ -426,4 +426,5 @@ modalSendButton.addEventListener('click', async () => {
         modalSendButton.disabled = false;
         modalSendButton.textContent = 'Отправить';
     }
+
 });
